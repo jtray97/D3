112c04bc0c45fe7819cb572700d322e5dfe5feba
unknown <jtray@outlook.es>
2018-08-03 11:21:12 -0600

    first commit

---
+++ react/presenting-d3/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import '../src/reset.css'
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>D3 presentation</h1>
+       
+      </div>
+    );
+  }
+}
+
+export default App;
